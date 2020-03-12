d37bd55f8c100fcc77a78ff5f375a907e6a937c1
Jeremy Greenwood <jgreenwd@hotmail.com>
2020-03-12 12:55:45 -0500

    Updates

---
+++ precache-manifest.08a8e2182b17c9b761aaf9bfaf391ea6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d030a78c3d3c70eaea5174f12b35baa0",
+    "url": "/musician-cards/index.html"
+  },
+  {
+    "revision": "8ea80273bc5cee937164",
+    "url": "/musician-cards/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2c37e8b2cbb1d0ce86a2",
+    "url": "/musician-cards/static/js/2.3f0a10a8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/musician-cards/static/js/2.3f0a10a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ea80273bc5cee937164",
+    "url": "/musician-cards/static/js/main.6f3c63fe.chunk.js"
+  },
+  {
+    "revision": "ca7c5b87a3f09cc67183",
+    "url": "/musician-cards/static/js/runtime-main.6fc93c3c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/musician-cards/static/media/logo.5d5d9eef.svg"
+  }
+]);
