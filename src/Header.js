055fedad15b38190e101968e3c57f3959145fd42
Jeremy Greenwood <jgreenwd@hotmail.com>
2020-03-12 15:11:43 -0500

    style: lower Header height

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header(props) {
     return (
-         <header style={{width: '360px'}}>
+         <header style={{width: '360px', height: '30px'}}>
             <h1 style={{textAlign: 'center'}}>Musician Cards</h1>
         </header>
     )
